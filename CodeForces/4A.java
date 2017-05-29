55c507458eb6a636cdb5305cfe7bb35facea25d1
GPhantom <shadytantawy@protonmail.com>
2017-05-29 22:14:26 +0200

    [+] 4A

---
+++ CodeForces/4A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class watermelon {
+	public static void main(String[]args){
+		System.out.println(w());
+	}
+	public static String w(){
+		Scanner scan = new Scanner(System.in);
+		int answer = scan.nextInt();
+		return ((answer != 2) && answer % 2 == 0)? "YES" : "NO";
+	}
+
+}
