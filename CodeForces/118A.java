9778eb9fb62b29f078161ebdfcdda3d19ca57632
GPhantom <shadytantawy@protonmail.com>
2017-05-29 22:16:12 +0200

    [+] 118A

---
+++ CodeForces/118A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class StringTask {
+	public static void main(String[]args){
+		w();
+	}
+	public static void w(){
+		Scanner scan = new Scanner(System.in);
+		String a = scan.nextLine();
+		String answer = a.toLowerCase();
+		for (int i = 0;i<answer.length();i++){
+			char c = answer.charAt(i);
+			if (c != 'a' && c != 'o' && c != 'y' && c != 'e' && c != 'u' && c != 'i'){
+				System.out.print("." + c);
+			}
+			else{
+				continue;
+			}
+		}
+	}
+
+}
